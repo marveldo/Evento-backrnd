b56d068f424993942baaaf744e2f067c1b744879
utibe solomon <utibesolomon6@gmail.com>
2024-11-01 10:47:51 +0100

    Commented on a feature

--- ws/middleware.py
+++ ws/middleware.py
@@ -3,6 +3,14 @@ from django.conf import settings
 
 
 def CorsHostValidator(application):
+    """Custom Host Validator to restrict my websocket from sources not present in cors allowed origins
+
+    Args:
+        application (_type_): Websocket application
+
+    Returns:
+        _type_: origin validator to ensure the connection is complete
+    """
     allowed_hosts = settings.ALLOWED_HOSTS
     if settings.DEBUG and not allowed_hosts:
         allowed_hosts = ["localhost", "127.0.0.1", "[::1]"]
