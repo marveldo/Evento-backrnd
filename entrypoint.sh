18d45401e08a56dce4ab6a849c875706b30a696b
utibe solomon <utibesolomon6@gmail.com>
2024-11-04 20:09:03 +0100

    Production changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-# gunicorn evento.wsgi:application --bind 0.0.0.0:8000 --reload
-python manage.py runserver 0.0.0.0:8000
+gunicorn evento.wsgi:application --bind 0.0.0.0:8000 --reload
