1a1dd13498186c43895b7fb3fccf9e8ec8fb6cbb
utibe solomon <utibesolomon6@gmail.com>
2024-11-04 21:05:15 +0100

    Production

--- backgroundentypoint.sh
+++ backgroundentypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python manage.py makemigrations --no-input
+python manage.py makemigrations users events --no-input
 python manage.py wait_for_db
 python manage.py migrate --no-input
 python manage.py process_tasks
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python manage.py makemigrations --no-input
+python manage.py makemigrations users events --no-input
 python manage.py migrate --no-input
 gunicorn evento.wsgi:application --bind 0.0.0.0:8000 --reload
