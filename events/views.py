c54f4599c7147febf000f6834815978d59c4ba3f
utibe solomon <utibesolomon6@gmail.com>
2024-11-13 17:03:37 +0100

    Added newe feature

--- events/views.py
+++ events/views.py
@@ -40,7 +40,7 @@ class EventViewset(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Create
         if self.request.GET.get('tag'):
             event_tag = EventTag.objects.filter(tag_name__icontains=self.request.GET.get('tag')).first()
             if event_tag :
-                return queryset.filter(tags = event_tag).order_by('-created')
+                return queryset.filter(tags = event_tag)
             elif self.request.GET.get('tag') == 'Recommended':
                 return queryset.filter(location__icontains = self.request.user.location)
         return queryset
